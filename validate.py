f46fafbe8d498cd60532b7981fa9f4ee178322c1
AWtnb <46807238+AWtnb@users.noreply.github.com>
2026-04-09 09:27:51 +0900

    update validation

--- validate.py
+++ validate.py
@@ -5,24 +5,22 @@ import json
 def main() -> None:
     all_ids = []
     all_display_names = []
-    all_names = []
-    all_abbrevs = []
+    tokens = []
 
     with open("journals.json", "r", encoding="utf-8") as f:
         content = json.load(f)
         for data in content:
             all_ids.append(data["id"])
             all_display_names.append(data["display_name"])
-            all_names.extend(data["names"])
+            tokens.extend(data["names"])
             abbrevs = [a for a in data["abbrevs"] if a != ""]
             if len(abbrevs) != 0:
-                all_abbrevs.extend(abbrevs)
+                tokens.extend(abbrevs)
 
     for name, item_list in {
         "id": all_ids,
         "display_name": all_display_names,
-        "name": all_names,
-        "abbrev": all_abbrevs,
+        "token": tokens,
     }.items():
         for item, count in collections.Counter(item_list).items():
             if count != 1:
