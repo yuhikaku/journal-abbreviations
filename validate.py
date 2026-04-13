835af2f900bfb6038eba5f288c131b74428741b4
AWtnb <46807238+AWtnb@users.noreply.github.com>
2026-04-13 16:38:54 +0900

    auto convert after validation

--- validate.py
+++ validate.py
@@ -1,22 +1,53 @@
 import collections
+import csv
 import json
 import re
+from pathlib import Path
+
+
+def convert_to_csv(json_path: str) -> None:
+    lines = [("name", "canonical_name", "journal_id", "abbrev_flag")]
+    with open(json_path, "r", encoding="utf-8") as f:
+        content = json.load(f)
+        for data in content:
+            journal_id = data["id"]
+            canonical_name = data["canonical_name"]
+            for name in data["names"]:
+                lines.append((name, canonical_name, journal_id, "0"))
+            for abbrev in data["abbrevs"]:
+                lines.append((abbrev, canonical_name, journal_id, "1"))
+
+    out_csv_path = Path(json_path).with_suffix(".csv")
+    with open(out_csv_path, "w", encoding="utf-8", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(lines)
 
 
 def main() -> None:
+    json_path = "journals.json"
     all_ids = []
     all_canonical_names = []
     tokens = []
 
-    with open("journals.json", "r", encoding="utf-8") as f:
+    with open(json_path, "r", encoding="utf-8") as f:
         content = json.load(f)
         for data in content:
             all_ids.append(data["id"])
-            all_canonical_names.append(data["canonical_name"])
-            tokens.extend(data["names"])
+
+            entry_tokens = []
+            entry_tokens.extend(data["names"])
             for abbrev in data["abbrevs"]:
                 if abbrev != "":
-                    tokens.append(abbrev)
+                    entry_tokens.append(abbrev)
+
+            canonical_name = data["canonical_name"]
+            if canonical_name not in entry_tokens:
+                raise RuntimeError(
+                    f"正規化名「{canonical_name}」として、 names にも abbrevs にも含まれていない文字列が指定されています"
+                )
+            all_canonical_names.append(canonical_name)
+
+            tokens.extend(entry_tokens)
 
     for name, item_list in {
         "id": all_ids,
@@ -32,7 +63,8 @@ def main() -> None:
         if not re.fullmatch(r"[A-Z0-9]{6}", suffix):
             raise ValueError(f"{jid}: IDの文字数が正しくありません")
 
-    print("問題は検出されませんでした")
+    print("問題は検出されませんでした。CSV形式に変換します。")
+    convert_to_csv(json_path)
 
 
 if __name__ == "__main__":
