beae96603d78e742fc1d1725e9ce01c40015c7c2
AWtnb <46807238+AWtnb@users.noreply.github.com>
2026-06-30 11:09:08 +0900

    update validation

--- validate.py
+++ validate.py
@@ -23,45 +23,57 @@ def convert_to_csv(json_path: str) -> None:
         writer.writerows(lines)
 
 
+def has_space_around(s: str) -> bool:
+    return s != s.strip()
+
+
 def main() -> None:
     json_path = "journals.json"
-    all_ids = []
-    all_canonical_names = []
-    tokens = []
+    id_stack = []
+    canonical_name_stack = []
+    keyword_stack = []
 
     with open(json_path, "r", encoding="utf-8") as f:
         content = json.load(f)
         for data in content:
-            all_ids.append(data["id"])
-
-            entry_tokens = []
-            entry_tokens.extend(data["names"])
-            for abbrev in data["abbrevs"]:
-                if abbrev != "":
-                    entry_tokens.append(abbrev)
-
+            data_id = data["id"]
             canonical_name = data["canonical_name"]
-            if canonical_name not in entry_tokens:
-                raise RuntimeError(
+            names = data["names"]
+            abbrevs = data["abbrevs"]
+
+            for elem in [data_id, canonical_name] + names + abbrevs:
+                if elem == "":
+                    raise ValueError(f"{data_id}に空文字が含まれています")
+                if elem.strip() == "":
+                    raise ValueError(f"{data_id}に空白文字のみの要素が含まれています")
+                if elem != elem.strip():
+                    raise ValueError(f"{data_id}「{elem}」の前後に空白があります")
+
+            id_stack.append(data_id)
+
+            keywords = names + abbrevs
+
+            if canonical_name not in keywords:
+                raise ValueError(
                     f"正規化名「{canonical_name}」として、 names にも abbrevs にも含まれていない文字列が指定されています"
                 )
-            all_canonical_names.append(canonical_name)
+            canonical_name_stack.append(canonical_name)
 
-            tokens.extend(entry_tokens)
+            keyword_stack.extend(keywords)
 
-    for name, item_list in {
-        "id": all_ids,
-        "canonical_name": all_canonical_names,
-        "token": tokens,
+    for name, stack in {
+        "ID": id_stack,
+        "正規化名称": canonical_name_stack,
+        "キーワード": keyword_stack,
     }.items():
-        for item, count in collections.Counter(item_list).items():
+        for item, count in collections.Counter(stack).items():
             if count != 1:
                 raise ValueError(f"{name}で「{item}」が{count}件重複しています。")
 
-    for jid in all_ids:
-        _, suffix = jid.split("-")
+    for data_id in id_stack:
+        _, suffix = data_id.split("-")
         if not re.fullmatch(r"[A-Z0-9]{6}", suffix):
-            raise ValueError(f"{jid}: IDの文字数が正しくありません")
+            raise ValueError(f"{data_id}: IDの文字数が正しくありません")
 
     print("問題は検出されませんでした。CSV形式に変換します。")
     convert_to_csv(json_path)
